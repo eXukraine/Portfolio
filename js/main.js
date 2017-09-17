65595581a57d8de25696fd89f0b315bc0c481dc3
Yamkovoi Alex <yamkovoi.a@gmail.com>
2017-09-18 00:23:08 +0300

    WEb site is DONE. Надо вылизать до готовности

--- js/main.js
+++ js/main.js
@@ -1,9 +1,6 @@
 jQuery(document).ready(function($) {
-/*$(document).ready(function(){
-    $('#go_next').click(function() {
-        $.scrollTo('#expertise');
-    });
-});*/
+
+// Page-scroll section
 
 $(function() {
     $(".page-scroll").bind("click", function(i) {
@@ -24,6 +21,46 @@ $(function() {
     })
 });
 
+
+
+                            //Modal window section
+
+var modal = document.getElementsByClassName("modal")[0];
+var modalBtn = document.getElementsByClassName('modal_btn')[0];
+var closeBtn = document.getElementsByClassName("closeBtn")[0];
+var contacts = document.getElementById("contacts");
+var contactsElem = document.getElementById("contacts").childNodes;
+var footer = document.getElementById("footer").childNodes;
+
+//Listen click
+
+modalBtn.addEventListener("click",openModal);
+closeBtn.addEventListener("click", closeModal);
+
+//Functions
+
+function openModal() {
+    //modal.style.visibility="visible";
+    //modalBtn.setAttribute("style", 'background-color: #ccc; transition: background-color: 5s');
+    modal.setAttribute("style", "visibility : visible; ");
+    contacts.style.background="#2A3A3F";
+    contactsElem[1].style.visibility = "hidden";
+    contactsElem[3].style.visibility = "hidden";
+    contactsElem[5].style.visibility = "hidden";
+    footer[1].style.visibility = "hidden";
+    footer[3].style.visibility = "hidden";
+}
+
+
+function closeModal() {
+    modal.style.visibility="hidden";
+    contacts.style.background="#FDCD3B";
+    contactsElem[1].style.visibility = "visible";
+    contactsElem[3].style.visibility = "visible";
+    contactsElem[5].style.visibility = "visible";
+    footer[1].style.visibility = "visible";
+    footer[3].style.visibility = "visible";
+}
 
 
 
